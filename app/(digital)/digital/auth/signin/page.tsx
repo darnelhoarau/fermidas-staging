d777da277f25fe1ee2bca4836cd10b263f690d42
Darnel Hoarau <darnelhoarau@gmail.com>
2026-06-20 19:25:04 +0400

    - Routed the [Get Started] btn to the sign up/in page (components/Header.tsx)
    - Fixed looping redirect issue on the sign in page (app/(digital)/digital/auth/signin/page.tsx)
    
    - Updated Node Modules that had 1 critial risk vulnerability (package-lock.json)

--- app/(digital)/digital/auth/signin/page.tsx
+++ app/(digital)/digital/auth/signin/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useState, Suspense } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { signIn } from 'next-auth/react';
